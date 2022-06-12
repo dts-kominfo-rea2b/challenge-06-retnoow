596543bbd04e7a99ee574d58b815720d64026e0b
Retno Wati <retnowati383@gmail.com>
2022-06-12 15:13:02 +0700

    read data

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,44 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const temp = [];
+
+  fs.readFile(file1, { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+      return fnCallback(err);
+    }
+
+    let getData = JSON.parse(data);
+    let getMessage = getData.message.split(" ");
+
+    temp.push(getMessage[1]);
+
+    fs.readFile(file2, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        return fnCallback(err);
+      }
+
+      let getData = JSON.parse(data);
+      let getMessage = getData[0].message.split(" ");
+
+      temp.push(getMessage[1]);
+
+      fs.readFile(file3, { encoding: "utf-8" }, (err, data) => {
+        if (err) {
+          return fnCallback(err);
+        }
+
+        let getData = JSON.parse(data);
+        let getMessage = getData[0].data.message.split(" ");
+
+        temp.push(getMessage[1]);
+
+        return fnCallback(err, temp);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
